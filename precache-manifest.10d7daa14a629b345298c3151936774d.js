bd2709f8d8a5fc560d837b0aef30a7eb587fd31d
Carlos Chininin <carloschininin@gmail.com>
2020-03-31 22:14:50 -0500

    Updates

---
+++ precache-manifest.10d7daa14a629b345298c3151936774d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14076aca7e93e64b6551bce53cdba29a",
+    "url": "/sisprogeov1.0/index.html"
+  },
+  {
+    "revision": "c8eb446660d866d11776",
+    "url": "/sisprogeov1.0/static/css/main.725ec094.chunk.css"
+  },
+  {
+    "revision": "1e024c12a6a6ee438aca",
+    "url": "/sisprogeov1.0/static/js/2.a5903bef.chunk.js"
+  },
+  {
+    "revision": "c8eb446660d866d11776",
+    "url": "/sisprogeov1.0/static/js/main.2850ff48.chunk.js"
+  },
+  {
+    "revision": "310e95d370616a6ae013",
+    "url": "/sisprogeov1.0/static/js/runtime~main.7fe1a1a9.js"
+  },
+  {
+    "revision": "7b69b5417063ca43646dcb941fafef5c",
+    "url": "/sisprogeov1.0/static/media/58d16c46cbe9f.7b69b541.jpeg"
+  }
+]);
